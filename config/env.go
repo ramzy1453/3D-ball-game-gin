cf29b63f2470959bb2f056fb6092768be3fa35ce
ramzy1453 <kemmounramzy93@gmail.com>
2024-11-30 23:21:10 +0100

    fixed bug prod

--- config/env.go
+++ config/env.go
@@ -1,9 +1,18 @@
 package config
 
 import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
 	"os"
 )
 
 func EnvMongoURI() string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	fmt.Println(os.Getenv("MONGO_URI"))
 	return os.Getenv("MONGO_URI")
 }
